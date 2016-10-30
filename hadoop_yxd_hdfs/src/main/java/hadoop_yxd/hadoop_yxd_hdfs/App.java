6ece0f47b370892189d05ce615f734d99a658918
yxdshine <659777399@qq.com>
2016-10-29 18:37:16 -0700

    ceshi

---
+++ hadoop_yxd_hdfs/src/main/java/hadoop_yxd/hadoop_yxd_hdfs/App.java
@@ -0,0 +1,13 @@
+package hadoop_yxd.hadoop_yxd_hdfs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hadoop_yxd_hdfs/src/main/java/hadoop_yxd/hadoop_yxd_hdfs/YxdHdfs.java
@@ -0,0 +1,12 @@
+package hadoop_yxd.hadoop_yxd_hdfs;
+
+public class YxdHdfs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		
+	}
+
+	
+}
