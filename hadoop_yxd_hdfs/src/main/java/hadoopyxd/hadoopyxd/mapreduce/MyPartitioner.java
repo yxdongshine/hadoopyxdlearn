7eb69fbe2ccb3eb5bb967612bef5ae46eebee2a7
yxdshine <659777399@qq.com>
2016-11-02 16:12:21 +0800

    方式

---
+++ hadoop_yxd_hdfs/src/main/java/hadoopyxd/hadoopyxd/mapreduce/MyPartitioner.java
@@ -0,0 +1,38 @@
+
+package hadoopyxd.hadoopyxd.mapreduce;  
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+/** 
+ * ClassName:MyPartitioner <br/> 
+ * Function: TODO (自定义分区). <br/> 
+ * Reason:   TODO (). <br/> 
+ * Date:     2016年11月2日 下午4:09:37 <br/> 
+ * @author   yxd 
+ * @version   
+ * @see       
+ */
+public class MyPartitioner extends Partitioner<LongWritable, IntWritable>{
+
+	/**
+	 * 根据每一行的偏移量 奇偶数来分区
+	 */
+	@Override
+	public int getPartition(LongWritable arg0, IntWritable arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+		
+		if( arg0.get() % 2 == 0) {  
+			arg0.set(0);  
+            return 0;  
+        } else {  
+        	arg0.set(1);  
+            return 1;  
+        }  
+		
+	}
+
+}
+  
