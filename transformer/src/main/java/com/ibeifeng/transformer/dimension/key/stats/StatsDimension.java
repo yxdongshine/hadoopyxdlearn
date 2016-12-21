c1f20ecbd5af7c1c14d870e0348ba642ef5a53e9
yxdshine <659777399@qq.com>
2016-12-21 18:16:23 +0800

    构建项目

---
+++ transformer/src/main/java/com/ibeifeng/transformer/dimension/key/stats/StatsDimension.java
@@ -0,0 +1,13 @@
+package com.ibeifeng.transformer.dimension.key.stats;
+
+import com.ibeifeng.transformer.dimension.key.BaseDimension;
+
+/**
+ * 组合维度类的基类
+ * 
+ * @author ibeifeng
+ *
+ */
+public abstract class StatsDimension extends BaseDimension {
+    // nothing
+}
---
+++ transformer/src/test/java/bf_transformer/TestHadoopHomeEnv.java
@@ -0,0 +1,9 @@
+package bf_transformer;
+
+public class TestHadoopHomeEnv {
+	public static void main(String[] args) {
+		// eclipse需要重启
+		System.out.println(System.getenv("HADOOP_HOME"));
+		System.out.println(System.getProperty("HADOOP_HOME"));
+	}
+}
